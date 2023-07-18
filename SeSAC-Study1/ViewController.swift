e4e67a8d7ed3e8e89b273e98929180816333f143
zundaeng <joony300@gmail.com>
2023-07-18 22:22:21 +0900

    Initial Commit

---
+++ SeSAC-Study1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC-Study1
+//
+//  Created by 정준영 on 2023/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
